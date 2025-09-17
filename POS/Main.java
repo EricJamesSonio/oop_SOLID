591ce169adb86c87820cc0d9720d04861386fe4d
EricJames <ericjamessonio7@gmail.com>
2025-09-17 21:15:25 +0800

    Initial commit

--- POS/Main.java
+++ POS/Main.java
@@ -1,7 +1,7 @@
 package POS;
 import POS.Employee.Management.EmployeeFactoryPlant;
 import POS.Employee.Management.UI.TUI.EmployeeManagementTUI;
-
+// Working palce
 public class Main {
     public static void main(String[] args) {
         EmployeeManagementTUI tui = EmployeeFactoryPlant.createEmployeeManagementTUI();
